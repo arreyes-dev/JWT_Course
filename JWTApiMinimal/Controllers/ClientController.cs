9ccfa463527030d6c15bb4d9ec13ed4a6484555f
Arturo Reyes <arreyes@tibagroup.com>
2025-06-11 16:08:15 -0600

    feat: Addded swagger and controllers

---
+++ JWTApiMinimal/Controllers/ClientController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWTApiMinimal.Controllers
+{
+    [ApiController]
+    [Route("Client")]
+    public class ClientController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("getList")]
+        public dynamic GetClientList()
+        {
+            return new[]
+                        {
+                    new
+                    {
+                        Id = 1,
+                        Name = "Alice Smith",
+                        Email = "alice@example.com"
+                    },
+                    new
+                    {
+                        Id = 2,
+                        Name = "Bob Johnson",
+                        Email = "bob@example.com"
+                    },
+                    new
+                    {
+                        Id = 3,
+                        Name = "Charlie Brown",
+                        Email = "charlie@example.com"
+                    }
+                };
+        }
+
+
+        [HttpDelete]
+        [Route("DeleteClient")]
+        public dynamic deleteCLient(string id) {
+            Microsoft.Extensions.Primitives.StringValues token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+
+            return new 
+            {
+                success = false,
+                message = "Token fail",
+                result = token
+            };
+
+        }
+
+    }
+}
--- JWTApiMinimal/Program.cs
+++ JWTApiMinimal/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Agregar servicios de Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// 2. Agrega contoladores ./Controllers/*
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// 3. Habilitar Swagger en tiempo de desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// 4. Usa los controladores agregados en el paso 2
+app.MapControllers();
+
 
 app.Run();
+
+
